e6af02df2d0efe6031e371e0fa212db5aa40e890
elidacon <elidacon@student.21-school.ru>
2025-03-17 23:21:43 +0500

    Move fetching vacancies logic to separate func

--- hh.py
+++ hh.py
@@ -58,28 +58,34 @@ async def fetch_vacancy_page(session, queue):
         page, per_page = await queue.get()
         logger.info(f"Fetch block ({page},{per_page}) from queue")
         try:
-            response = await session.get(
-                url=VACANCIES_URL,
-                params={"page": page, "per_page": per_page},
-                headers=HEADERS,
-            )
-            if response.status != 200:
-                logger.error(
-                    f"Error fetching {VACANCIES_URL} with page={page} per_page={per_page}: {response.status}\n{await response.text()}"
+            vacancies = await fetch_vacancies_from_page(session, page, per_page)
+            for idx, vacancy in enumerate(vacancies):
+                logger.info(
+                    f"Page={page} idx={idx}: {vacancy['id']} {vacancy['name']} {vacancy['employer']['name']}"
                 )
-            else:
-                response_json = await response.json()
-                logger.info(f"Page={page} got {len(response_json['items'])} vacancies")
-                for idx, vacancy in enumerate(response_json["items"]):
-                    logger.info(
-                        f"Page={page} idx={idx}: {vacancy['id']} {vacancy['name']} {vacancy['employer']['name']}"
-                    )
         except Exception as e:
             logger.error(
                 f"Fetch block ({page},{per_page}) from queue finished with error {str(e)}"
             )
         finally:
             queue.task_done()
+
+
+async def fetch_vacancies_from_page(session, page, per_page):
+    response = await session.get(
+        url=VACANCIES_URL,
+        params={"page": page, "per_page": per_page},
+        headers=HEADERS,
+    )
+    if response.status != 200:
+        logger.error(
+            f"Error fetching {VACANCIES_URL} with page={page} per_page={per_page}: {response.status}\n{await response.text()}"
+        )
+        return []
+    else:
+        response_json = await response.json()
+        logger.info(f"Page={page} got {len(response_json['items'])} vacancies")
+        return response_json["items"]
 
 
 async def main(workers_num: int):
