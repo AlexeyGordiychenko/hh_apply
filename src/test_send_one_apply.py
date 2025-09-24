be1ad35025c57d4901290606dc282bd0ea6c7955
elidacon <elidacon@student.21-school.ru>
2025-09-24 21:08:53 +0500

    Add test_send_one_apply.py

---
+++ src/test_send_one_apply.py
@@ -0,0 +1,39 @@
+import argparse
+import aiohttp
+from src.send_applies import SearchType, add_apply_to_notion, apply_to_vacancy, get_vacancies_response
+
+def parse_args() -> int:
+    parser = argparse.ArgumentParser(
+        description="A script to apply to one vacancy on hh.ru for testing the script"
+    )
+    parser.add_argument(
+        "-s",
+        "--search",
+        type=SearchType,
+        choices=list(SearchType),
+        default=SearchType.SIMILAR,
+        required=True,
+        help="Search similar or query",
+    )
+    args = parser.parse_args()
+    return args.search
+
+async def send(search):
+    async with aiohttp.ClientSession() as session:
+        response_json = await get_vacancies_response(session=session, page=0, search=search)
+        vacancy = response_json["items"][0]
+        negotiation_url = await apply_to_vacancy(session=session, vacancy_id=vacancy["id"], logger_msg="")
+        if negotiation_url:
+            await add_apply_to_notion(
+                session=session,
+                company=vacancy["employer"]["name"],
+                position=vacancy["name"],
+                url=vacancy["alternate_url"],
+                negotiation_url=negotiation_url,
+                logger_msg="",
+            )
+
+if __name__ == "__main__":
+    import asyncio
+    search = parse_args()
+    asyncio.run(send(search))
